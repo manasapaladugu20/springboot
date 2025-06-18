95dc5ba6f5685eed3c3bc420aaa68b562d1491a3
manasapaladugu <manasapaladugu20@gmail.com>
2025-06-17 20:20:29 -0500

    Add a change in line 12

--- ecom-backend/src/main/java/com/ecommerce/EcomBackendApplication.java
+++ ecom-backend/src/main/java/com/ecommerce/EcomBackendApplication.java
@@ -9,6 +9,7 @@ public class EcomBackendApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(EcomBackendApplication.class, args);
+        //change-1
     }
 
 
